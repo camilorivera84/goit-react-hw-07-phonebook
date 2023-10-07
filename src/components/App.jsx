b3cfa5dfca54eee327b249c9d60fd33274bba092
camilorivera84 <110140389+camilorivera84@users.noreply.github.com>
2023-10-07 00:16:23 -0500

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  fetchContacts,
+  selectIsLoading,
+  selectError,
+} from '../redux/ContactsSlice';
+
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>
+        Contacts
+        {isLoading && <span> [Loading...]</span>}
+        {error && <span> [Error:{error}.not Found]</span>}
+      </h2>
+      <Filter />
+      <ContactList />
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactsReducer from '../redux/ContactsSlice';
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+});
